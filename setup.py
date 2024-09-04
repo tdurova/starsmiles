0cbbf0649c71cb6fbeabdfc422d04edc4ce7ca46
Enric Armengol <enricarmengol@hotmail.com>
2024-09-04 11:47:30 +0200

    package structure

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ requirements = [x.strip() for x in content if "git+" not in x]
 setup(name='starsmiles',
       version="0.0.0",
       description="Star Smiles Model (First local test)",
-      license="MIT",???
+      license="MIT",#???
       author="Tanja Durova",
-      author_email="contact@lewagon.org",???
+      author_email="tatyana.durova@gmail.com",
       #url="https://github.com/tdurova/starsmiles",
       install_requires=requirements,
       packages=find_packages(),
-      test_suite="tests", ???
+      test_suite="tests", #???
       # include_package_data: to install data from MANIFEST.in ???
       include_package_data=True,
       zip_safe=False)
