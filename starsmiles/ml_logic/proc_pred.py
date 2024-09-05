3a56b17ea4fe4a77425717e02530a7d0a1f42cdf
Tanja Durova <tatyana.durova@gmail.com>
2024-09-05 13:39:47 +0200

    fix proc_pred

--- starsmiles/ml_logic/proc_pred.py
+++ starsmiles/ml_logic/proc_pred.py
@@ -1,20 +1,25 @@
+import os
 import numpy as np
-
 from tensorflow.keras.utils import img_to_array
 from PIL import Image
 from tensorflow.keras.preprocessing.image import smart_resize
 from tensorflow.image import rgb_to_grayscale
-import numpy as np
 import tensorflow as tf
 
-# Load your pre-trained model (adjust the path as necessary)
-model = tf.keras.models.load_model('models/model.keras')
+# Load the model path from the environment variable
+model_path = os.getenv('MODEL_PATH')
 
-def load_image(path_to_image: str)-> Image:
+# Check if the model file exists
+if not model_path or not os.path.exists(model_path):
+    raise FileNotFoundError(f"Model file not found: {model_path}")
+
+# Load your pre-trained model
+model = tf.keras.models.load_model(model_path)
+
+def load_image(path_to_image: str) -> Image:
     return Image.open(path_to_image)
 
 def preproc_image(image: Image):
-
     img = img_to_array(image)
 
     if img.shape[:2] != (64, 64):
@@ -26,23 +31,21 @@ def preproc_image(image: Image):
 
 def load_model():
     from tensorflow.keras.models import load_model
+    return load_model(model_path)
 
-    return load_model('/home/enric/code/tdurova/starsmiles/model/model.keras',)
-
-def predict(img, model=load_model())->None:
-
+def predict(img, model=load_model()) -> None:
     p = np.expand_dims(img, axis=0)
-
     pred = model.predict(p)
 
     class_names = ['Cavity', 'Fillings', 'Impacted Tooth', 'Implant', 'Normal']
-
     threshold = 0.5
-    for i in range(len(pred[0])):
-        print(f'Probability of {class_names[i]}: {round(100*pred[0,i],2)}%')
+    prediction = None
 
-        if pred[0,i]>threshold:
-            threshold=pred[0,i]
+    for i in range(len(pred[0])):
+        print(f'Probability of {class_names[i]}: {round(100 * pred[0, i], 2)}%')
+
+        if pred[0, i] > threshold:
+            threshold = pred[0, i]
             prediction = class_names[i]
 
     if prediction is not None:
